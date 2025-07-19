55bfb4bbe83d83a48300ed76cc011199023dd80b
Shehab Elkhaligy <shehabelkhaligy100@gmail.com>
2025-07-19 20:30:10 +0300

    Initial commit

---
+++ config/tailwind.config.js
@@ -0,0 +1,39 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: [
+//       './public/*.html',
+//       './app/helpers/**/*.rb',
+//       './app/javascript/**/*.js',
+//       './app/views/**/*',
+//     ],
+//     theme: {
+//       extend: {
+//         colors: {
+//           primary: {
+//             DEFAULT: '#4A6CF7',
+//             dark: '#3C56DD',
+//           },
+//           secondary: {
+//             DEFAULT: '#13C296',
+//             dark: '#0FB086',
+//           },
+//           'body-color': '#637381',
+//           'body-color-dark': '#AEB7C0',
+//           black: '#212B36',
+//           'gray-1': '#F8F9FA',
+//           'gray-2': '#F1F3F6',
+//           'gray-3': '#DEE2E7',
+//           'gray-4': '#C1C7CD',
+//           'gray-5': '#A4ACB4',
+//           'gray-6': '#8B9399',
+//           'gray-7': '#637381',
+//           'dark-1': '#1D2144',
+//           'dark-2': '#161A42',
+//           warning: '#FBBF24',
+//           success: '#06D6A0',
+//           danger: '#EF4444',
+//         },
+//       },
+//     },
+//     plugins: [],
+//   }
