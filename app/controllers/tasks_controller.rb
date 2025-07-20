6fccc795795dd7ce680a966db31d6b11a66be4b0
Shehab Elkhaligy <shehabelkhaligy100@gmail.com>
2025-07-20 21:14:36 +0300

    add task viewing feature

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,52 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.order(created_at: :desc)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "Task was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    puts "=== DEBUG INFO ==="
+    puts "params: #{params.inspect}"
+    puts "task_params: #{task_params}"
+    puts "=================="
+
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "Task was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice: "Task was successfully deleted."
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title, :description, :completed, :priority, :due_date)
+    end
+end
